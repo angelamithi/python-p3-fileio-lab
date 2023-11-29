560fc78a7cc775fbecdf960def57e0baaa6eae4d
+ <+>
2023-11-29 21:25:57 +0300

    completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,11 +1,11 @@
 def write_file(file_path, content):
-    with open(f'{file_path}.txt', 'w') as file:
+    with open(f'{file_path}.txt', mode='w') as file:
         file.write(content)
 
 def append_file(file_path, content):
-    with open(f'{file_path}.txt', 'a') as file:
+    with open(f'{file_path}.txt', mode='a') as file:
         file.write(content)
 
 def read_file(file_path):
-    with open(f'{file_path}.txt', 'r') as file:
+    with open(f'{file_path}.txt', mode='r') as file:
         return file.read()
